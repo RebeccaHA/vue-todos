9af99541e31c0fcad414b8f2c76b8153345f4c08
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-08 21:36:00 +0000

    got to know file structure and setup

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <TodoItem></TodoItem>
+</template>
+<script>
+import TodoItem from "./components/TodoItem.vue";
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoItem
+  }, 
+  data()
+};
+</script>
+
