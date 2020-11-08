665fbb1b21646ef0dc1515c4b86e21fa2e2c88d3
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-08 22:05:00 +0000

    added form with v-model

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input id="name" v-model="name" />
+      <br />
+      <textarea id="description" v-model="description"></textarea>
+      <select id="importance" v-model="importance">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoForm",
+
+  data() {
+    return {
+      title: "",
+      description: "",
+      name: "",
+      createdDate: "",
+      importance: null
+    };
+  }
+};
+</script>
